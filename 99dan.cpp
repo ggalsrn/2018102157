5c945388590433d2198a8db1b415969abb5a6f28
ggalsrn <92130682+ggalsrn@users.noreply.github.com>
2021-10-08 11:45:33 +0900

    Update 99dan.cpp

--- 99dan.cpp
+++ 99dan.cpp
@@ -5,7 +5,7 @@ int main() {
 	for(int i=1; i <= 8; i++){
 		for(int j=1; j <=9;  j++){
 
-			cout << i << "*" << j << "=" << i*j;
+			cout << i << "*" << j << "=" << i*j << endl;
 		}
 			cout << endl;
 	}
